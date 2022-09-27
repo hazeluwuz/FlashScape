f5dd0669f485ac12f4be94cc0fa732432d22bcc3
Hazel Guzzetti <viizionprime@gmail.com>
2022-09-27 18:48:39 -0400

    more styling complete for splashpage

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -1,8 +1,18 @@
+import "./SplashPage.css";
 function SplashPage() {
   return (
     <div className="main-splash-container">
-      <div className="splash-container">
-        <h1>SPLASH PAGE</h1>
+      <div className="carousel-overlay"></div>
+      <div className="splash-details">
+        <h1 className="splash-header">Rise to</h1>
+        <h1 className="splash-header">your challenge.</h1>
+        <h2 className="splash-subheader">
+          Flashcards for <b>serious learners.</b>
+        </h2>
+      </div>
+      <div className="carousel-container">
+        {/* Plan to implement carousel similar to BrainScape Eventually */}
+        <div className="carousel-item"></div>
       </div>
     </div>
   );
