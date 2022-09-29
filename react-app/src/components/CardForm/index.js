2ac1f72cb53b541386071379267ea9d11db30d79
Hazel Guzzetti <viizionprime@gmail.com>
2022-09-28 20:49:04 -0400

    added deletion icon for decks, and implemented very baasic form for adding/editing cards

--- react-app/src/components/CardForm/index.js
+++ react-app/src/components/CardForm/index.js
@@ -2,6 +2,7 @@ import { useDispatch } from "react-redux";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { createNewCard, updateCardById } from "../../store/card";
+import "./CardForm.css";
 function CardForm({ edit, card }) {
   const [question, setQuestion] = useState(card?.question || "");
   const [answer, setAnswer] = useState(card?.answer || "");
@@ -34,22 +35,34 @@ function CardForm({ edit, card }) {
     console.log(res);
   };
   return (
-    <form onSubmit={edit ? handleEdit : handleCreate}>
-      <label>Question:</label>
-      <input
-        type="text"
-        value={question}
-        onChange={(e) => setQuestion(e.target.value)}
-        required
-      />
-      <label>Answer:</label>
-      <input
-        type="text"
-        value={answer}
-        onChange={(e) => setAnswer(e.target.value)}
-        required
-      />
-      <button type="submit">{edit ? "Edit Card" : "Create Card"}</button>
+    <form onSubmit={edit ? handleEdit : handleCreate} className="card-form">
+      <div className="card-form-container">
+        <div className="input-container">
+          <input
+            type="text"
+            value={question}
+            placeholder=" "
+            className="text-input"
+            onChange={(e) => setQuestion(e.target.value)}
+            required
+          />
+          <label>Question</label>
+        </div>
+        <div className="input-container">
+          <input
+            type="text"
+            className="text-input"
+            value={answer}
+            placeholder=" "
+            onChange={(e) => setAnswer(e.target.value)}
+            required
+          />
+          <label>Answer</label>
+        </div>
+        <button type="submit" className="round-button card-submit-button">
+          {edit ? "Edit Card" : "Create Card"}
+        </button>
+      </div>
     </form>
   );
 }
--- react-app/src/components/DeckCard/index.js
+++ react-app/src/components/DeckCard/index.js
@@ -1,14 +1,34 @@
-import { NavLink } from "react-router-dom";
-import { useRouteMatch } from "react-router-dom";
+import { NavLink, useRouteMatch } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { deleteDeckById } from "../../store/deck";
+import { useState } from "react";
 import "./DeckCard.css";
 
 function DeckCard({ deck }) {
+  const [isShown, setIsShown] = useState(false);
   const { url } = useRouteMatch();
+  const dispatch = useDispatch();
   if (!deck) return null;
+
+  const handleDelete = (e) => {
+    e.preventDefault();
+    dispatch(deleteDeckById(deck.id));
+  };
+
   return (
-    <NavLink to={`${url}/${deck.id}/browse`} className="deck-card-container">
-      <div>
+    <NavLink
+      onMouseEnter={() => setIsShown(true)}
+      onMouseLeave={() => setIsShown(false)}
+      to={`${url}/${deck.id}/browse`}
+      className="deck-card-container"
+    >
+      <div className="deck-card-inner-container">
         <h2 className="deck-card-name">{deck.name}</h2>
+        {isShown && (
+          <button onClick={handleDelete} className="deck-delete-button">
+            <i class="fa-solid fa-trash deck-delete-icon"></i>
+          </button>
+        )}
       </div>
     </NavLink>
   );
