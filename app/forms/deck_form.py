7844b3d36a8dcc3a378644ae337b5e2de1ac06f8
Hazel Guzzetti <viizionprime@gmail.com>
2022-10-03 13:16:23 -0400

    added backend validator for name on deck creation

--- app/forms/deck_form.py
+++ app/forms/deck_form.py
@@ -2,6 +2,12 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField
 from wtforms.validators import DataRequired, Email, ValidationError
 
+def validate_name(form, field):
+    if len(field.data) < 5:
+        raise ValidationError('Name must be at least 5 characters long.')
+    elif len(field.data) > 50:
+        raise ValidationError('Name must be less than 50 characters long.')
+
 class DeckForm(FlaskForm):
-  name = StringField('name', validators=[DataRequired()])
+  name = StringField('name', validators=[DataRequired(), validate_name])
   class_id = IntegerField('class_id', validators=[DataRequired()])
