51f714086c5af497218c9ea91ddc0240007412e3
Hazel Guzzetti <viizionprime@gmail.com>
2022-10-04 21:52:38 -0400

    added confirmation for deletion of class

---
+++ react-app/src/components/ClassDeleteModal/ClassDelete.js
@@ -0,0 +1,36 @@
+import { deleteClassById } from "../../store/class";
+import { useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import "./ClassDelete.css";
+function ClassDelete({ closeModal, classData }) {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const handleDelete = async (e) => {
+    e.preventDefault();
+    const res = await dispatch(deleteClassById(classData.id));
+    if (res.ok) {
+      history.push("/dashboard");
+    }
+  };
+
+  return (
+    <div className="modal-delete-warning">
+      <h3 className="class-warning-text">
+        {`You are about to remove the class ${classData.name} from your library. Are you sure you wish to proceed?`}
+      </h3>
+      <div className="modal-warning-actions">
+        <button
+          className="cancel-button round-button"
+          onClick={() => closeModal()}
+        >
+          Cancel
+        </button>
+        <div className="class-confirm-delete" onClick={handleDelete}>
+          Yes, remove class
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ClassDelete;
