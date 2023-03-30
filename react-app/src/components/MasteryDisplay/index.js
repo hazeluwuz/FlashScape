4706a6f451fbe56766a47a22e144e54e208bb99e
Hazel Guzzetti <viizionprime@gmail.com>
2023-03-30 02:28:38 -0400

    refactored mastery to reduce repetition

--- react-app/src/components/MasteryDisplay/index.js
+++ react-app/src/components/MasteryDisplay/index.js
@@ -4,6 +4,7 @@ import "./MasteryDisplay.css";
 
 function MasteryDisplay({ card, setFlipped, colors }) {
   const dispatch = useDispatch();
+
   const handleMasteryChange = (e, mastery) => {
     e.preventDefault();
     const payload = {
@@ -24,47 +25,20 @@ function MasteryDisplay({ card, setFlipped, colors }) {
         How well did you know this?
       </div>
       <div className="mastery-select-container">
-        <div>
-          <button
-            onClick={(e) => handleMasteryChange(e, 1)}
-            className="mastery-select-button"
-            style={{ backgroundColor: colors[1] }}
-          >
-            1
-          </button>
-          <div className="mastery-container-labels">Not at all</div>
-        </div>
-        <button
-          onClick={(e) => handleMasteryChange(e, 2)}
-          className="mastery-select-button"
-          style={{ backgroundColor: colors[2] }}
-        >
-          2
-        </button>
-        <button
-          onClick={(e) => handleMasteryChange(e, 3)}
-          className="mastery-select-button"
-          style={{ backgroundColor: colors[3] }}
-        >
-          3
-        </button>
-        <button
-          onClick={(e) => handleMasteryChange(e, 4)}
-          className="mastery-select-button"
-          style={{ backgroundColor: colors[4] }}
-        >
-          4
-        </button>
-        <div>
-          <button
-            onClick={(e) => handleMasteryChange(e, 5)}
-            className="mastery-select-button"
-            style={{ backgroundColor: colors[5] }}
-          >
-            5
-          </button>
-          <div className="mastery-container-labels">Perfectly</div>
-        </div>
+        {[1, 2, 3, 4, 5].map((num) => (
+          <div>
+            <button
+              onClick={(e) => handleMasteryChange(e, num)}
+              className="mastery-select-button"
+              style={{ backgroundColor: colors[num] }}
+            >
+              {num}
+            </button>
+            <div className="mastery-container-labels">
+              {num === 1 ? "Not at all" : num === 5 ? "Perfectly" : ""}
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
