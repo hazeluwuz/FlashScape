ce4c4b62821ed8deb4cc30d7f5a7a88c64994d8d
Hazel Guzzetti <viizionprime@gmail.com>
2023-04-18 00:33:32 -0400

    adjusted migrations to incorporate schema

--- migrations/versions/20220927_125501_.py
+++ migrations/versions/20220927_125501_.py
@@ -8,6 +8,10 @@ Create Date: 2022-09-27 12:55:01.924363
 from alembic import op
 import sqlalchemy as sa
 
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
+
 
 # revision identifiers, used by Alembic.
 revision = 'b0335f9d7d65'
@@ -25,6 +29,8 @@ def upgrade():
     sa.ForeignKeyConstraint(['owner_id'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
+    if environment == "production":
+        op.execute(f"ALTER TABLE classes SET SCHEMA {SCHEMA};")
     # ### end Alembic commands ###
 
 
--- migrations/versions/20220927_150421_added_deck_table.py
+++ migrations/versions/20220927_150421_added_deck_table.py
@@ -8,6 +8,10 @@ Create Date: 2022-09-27 15:04:21.153569
 from alembic import op
 import sqlalchemy as sa
 
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
+
 
 # revision identifiers, used by Alembic.
 revision = 'e13c20cfd02c'
@@ -25,6 +29,8 @@ def upgrade():
     sa.ForeignKeyConstraint(['class_id'], ['classes.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
+    if environment == "production":
+        op.execute(f"ALTER TABLE <table_name> SET SCHEMA {SCHEMA};")
     # ### end Alembic commands ###
 
 
